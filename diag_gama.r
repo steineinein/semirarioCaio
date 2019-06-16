9938a9b480aa0968743256779ec108cb585593ce
gabriel stein <steingps@gmail.com>
2019-06-16 18:12:52 -0300

    commit

--- diag_gama.r
+++ diag_gama.r
@@ -43,10 +43,10 @@ b <- min(td)
 plot(fitted(fit.model),h,xlab="Valor Ajustado", ylab="Medida h", pch=16)
 #identify(fitted(fit.model), h, n=1)
 #
-plot(di,xlab="Indice", ylab="Dist?ncia de Cook", pch=16)
+plot(di,xlab="Indice", ylab="Distância de Cook", pch=16)
 #identify(di, n=2)
 #
-plot(fitted(fit.model),td,xlab="Valor Ajustado", ylab="Res?duo Componente do Desvio",
+plot(fitted(fit.model),td,xlab="Valor Ajustado", ylab="Resíduo Componente do Desvio",
 ylim=c(b-1,a+1),pch=16)
 abline(2,0,lty=2)
 abline(-2,0,lty=2)
@@ -56,7 +56,7 @@ w <- fit.model$weights
 eta <- predict(fit.model)
 z <- eta + resid(fit.model, type="pearson")/sqrt(w)
 plot(predict(fit.model),z,xlab="Preditor Linear", 
-ylab="Vari?vel z", pch=16)
+ylab="Variável z", pch=16)
 par(mfrow=c(1,1))
 #------------------------------------------------------------#
 
